da52a072eaa177071802cc351d6ec0688ebdc1e9
2chenj <habinshutithierry@gmail.com>
2020-01-17 04:39:13 +0100

    going on with iterators

--- tp1/iterateurs.ml
+++ tp1/iterateurs.ml
@@ -8,6 +8,26 @@ affiche_liste_entiers [1;2;3;4;5;6;7;8;9];;
 let count cible l = 
         List.fold_left  (fun a b -> if cible = b then a+1 else a) 0 l
 ;;
-print_string"\n----------count---------------\n";;
+print_endline "\n----------count---------------\n";;
 print_int (count 5 [1;2;3;4;5;5;5;7;8]);;
+
+let rec funOuter l=
+        let rec funInnerRec l2 acc = 
+              match l2 with 
+              [] -> acc
+              |h::tail ->funInnerRec tail (h::acc)
+        in funInnerRec l []
+;;
+
+let rec funrec l acc = 
+        match l with 
+        []-> acc 
+        |h::tail -> funrec tail (h::acc)
+;;
+
+let flatten l =
+        List.fold_right (fun l1 acc -> (funrec l1 acc) )l [];;
+print_newline ;;
+print_endline " affichage de la liste flatternisé";;
+affiche_liste_entiers (flatten [[1;2];[];[3;4;5];[6;7;8;9]]);;
 
